25f57c51bc2d69444ddccc545e37fe486cd88a66
‘“liujun <1650531160@qq.com>
2019-02-14 16:36:00 +0800

    登录功能实现完毕

--- src/main.js
+++ src/main.js
@@ -2,6 +2,22 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 引入global全局文件控制样式
+import './assets/css/global.css'
+import 'element-ui/lib/theme-chalk/index.css'
+// 引入图标css样式
+import './assets/fonts/iconfont.css'
+// 引入element-ui组件库模块
+import ElementUI from 'element-ui'
+// 把element-ui注册给vue
+Vue.use(ElementUI)
+// 引入axios并做相关配置
+import axios from 'axios'
+// axios请求根地址
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 给axios配置给vue的￥http成员
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+// 路由中配置使用Login组件
+import Login from '@/components/Login'
+
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    { path: '/', component: HelloWorld },
+    { path: '/login', component: Login },
+    { path: '/home', component: Home}
   ]
 })
