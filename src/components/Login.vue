65889002255f23cf269cbbac6078d5aca2315d32
‘“liujun <1650531160@qq.com>
2019-02-15 18:56:19 +0800

    用户登录功能实现完毕

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -11,8 +11,8 @@
             <i slot="prefix" class="iconfont icon-user"></i>
           </el-input>
         </el-form-item>
-        <el-form-item prop="password">
-          <el-input v-model="loginForm.password">
+        <el-form-item   prop="password">
+          <el-input v-model="loginForm.password" type="password">
             <i slot="prefix" class="iconfont icon-3702mima"></i>
           </el-input>
         </el-form-item>
